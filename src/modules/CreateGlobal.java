7f5b0ab5fe452c070fcccb1b7e038cdd32b10259
avishek shah <shahliberty@gmail.com>
2014-05-08 14:35:55 +0530

    Create GlobalClass
    Takes all the global variables and put them in a class called "GlobalVar"

--- src/modules/CreateGlobal.java
+++ src/modules/CreateGlobal.java
@@ -2,10 +2,39 @@ import java.util.ArrayList;
 
 class CreateGlobal{
 
-private ArrayList<String> template = new Arraylist<String>();
-
-public ArrayList<String> doCreateGlobal(ArrayList<String> srcList){
-  template.add("");
-  template.add("");
-  template.add("");
-  template.add("");
+    private ArrayList<String> template = new Arraylist<String>();
+    int i, count =0,startpos;
+    public ArrayList<String> doCreateGlobal(ArrayList<String> srcList){
+        template.add("public");
+        template.add("class");
+        template.add("GlobalVar");
+        template.add("{");
+        template.add("public");
+        template.add("}");
+        for(i=0;i<srcList.size();i++)
+        {
+            if(srcList.get(i).equals("{")
+              count ++;
+            else if(srcList.get(i).equals("}")
+              count --;
+            else if(count == 0 && (srcList.get(i).equals("int") || srcList.get(i).equals("float") || srcList.get(i).equals("double") || srcList.get(i).equals("char"))
+            { 
+              startpos=i;
+              do
+              {
+                template.add(srcList.get(i));
+                srcList.remove(i);
+              
+              }while(!srcList.get(i).equals(";"));
+            }
+  
+  }for(int k = 0; k < template.size();k++)
+  {
+  while(template.isEmpty())
+    { 
+      
+      srcList.add(startpos++,template.get(k));
+    }
+  }
+  }
+  }
