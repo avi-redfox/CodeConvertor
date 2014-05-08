5c6afd3109ff2ea4c4267b6b4c7e7dab4a705f51
avishek shah <shahliberty@gmail.com>
2014-05-08 14:10:37 +0530

    Create CreateGlobal.java

---
+++ src/modules/CreateGlobal.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class CreateGlobal{
+
+private ArrayList<String> template = new Arraylist<String>();
+
+public ArrayList<String> doCreateGlobal(ArrayList<String> srcList){
+  template.add("");
+  template.add("");
+  template.add("");
+  template.add("");
